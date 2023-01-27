8053e7eec210680c05d6ac58b65051e0ae13034a
NameLesssNerd <itsmesushant20xx@gmail.com>
2023-01-27 14:31:09 +0545

    initial version release

---
+++ index.js
@@ -0,0 +1,55 @@
+const validateText = "`~ 1234567890-=!@#$%^&*()_+[]{};':,./<>?\|";
+const afterValidateText = '"';
+const validateNumberCheckLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const validateNumberCheckSpecial = "`~!@#$%^&*()_+-={[}}|\:;',<.>/?"
+const Eletter = (options) => {
+    if ((!options) == "") {
+        let totalLetter = "";
+        const seperateAll = options.split("");
+        seperateAll.map((letter) => {
+            validateText.includes(letter) ? "" : (afterValidateText.includes(letter) ? "" : (totalLetter += letter));
+        })
+        refinedSearch = totalLetter.toLowerCase()
+        return refinedSearch;
+    } else {
+        return 0;
+    }
+}
+const Enumber = (options) => {
+    if (!options == "") {
+        let totalNumber = "";
+        const lowerLetter = Eletter(validateNumberCheckLetter);
+        const seperateAll = options.split("");
+        seperateAll.map((number) => {
+            validateNumberCheckLetter.includes(number) ? "" : (lowerLetter.includes(number) ? "" : (validateNumberCheckSpecial.includes(number)) ? "" : totalNumber += number);
+        })
+        return totalNumber
+    } else {
+        return 0;
+    }
+}
+const upperConvert = (options) => {
+    if (!options == "") {
+        const upperLetter = options.toUpperCase();
+        return upperLetter;
+    } else {
+        return 0
+    }
+}
+const Especial = (options) => {
+    if (!options == "") {
+        let totalSpecial = "";
+        const letterCheck = Eletter(options);
+        const numberCheck = Enumber(options);
+        const UpperCheck = upperConvert(letterCheck);
+        const seperateAll = options.split("");
+        seperateAll.map((special) => {
+            letterCheck.includes(special) ? "" : (numberCheck.includes(special) ? "" : (UpperCheck.includes(special) ? "" : (totalSpecial += special)));
+        })
+        return totalSpecial;
+    } else {
+        return 0;
+    }
+}
+
+module.exports = { Eletter, Enumber, Especial };
